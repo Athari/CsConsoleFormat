fb0e773980c4ce9ddbe9ddc71d391ee611df9378
Athari <athari.p@gmail.com>
2018-02-21 01:06:35 +0300

    Switched main project from Net 4.0 to multi-targeted .NET 4.0 + .NET Standard 2.0.

--- Alba.CsConsoleFormat/Properties/AssemblyInfo.cs
+++ Alba.CsConsoleFormat/Properties/AssemblyInfo.cs
@@ -1,24 +1,11 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows.Markup;
 
-[assembly: AssemblyTitle("Alba.CsConsoleFormat")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Alba.CsConsoleFormat")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("4ad934e0-78da-4356-917f-130742476086")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: XmlnsPrefix("urn:alba:cs-console-format", "a")]
 [assembly: XmlnsDefinition("urn:alba:cs-console-format", "Alba.CsConsoleFormat")]
