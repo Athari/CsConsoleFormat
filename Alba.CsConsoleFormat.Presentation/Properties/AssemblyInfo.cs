eee22794a979f4d3c24861c82d6799bb100f2987
Athari <athari.p@gmail.com>
2018-02-22 15:52:40 +0300

    Converted Alba.CsConsoleFormat.Presentation project to new format, added building of NuGet package. WPF project system based on https://github.com/aienabled/WpfNewProjectSystemSample.

--- Alba.CsConsoleFormat.Presentation/Properties/AssemblyInfo.cs
+++ Alba.CsConsoleFormat.Presentation/Properties/AssemblyInfo.cs
@@ -1,26 +1,13 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
-
-[assembly: AssemblyTitle("Alba.CsConsoleFormat.Presentation")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Alba.CsConsoleFormat.Presentation")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("cd3fe971-fd37-49c7-a6fc-fdc3a478cfcd")]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: XmlnsPrefix("urn:alba:cs-console-format", "a")]
 [assembly: XmlnsDefinition("urn:alba:cs-console-format", "Alba.CsConsoleFormat.Presentation")]
