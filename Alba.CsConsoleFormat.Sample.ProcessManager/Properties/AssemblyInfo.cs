4f0ed3a2ba7f724c90d26deff2fc286230c2164d
Athari <athari.p@gmail.com>
2018-02-22 03:06:12 +0300

    Converted Alba.CsConsoleFormat.Sample.ProcessManager project to new format.

--- Alba.CsConsoleFormat.Sample.ProcessManager/Properties/AssemblyInfo.cs
+++ Alba.CsConsoleFormat.Sample.ProcessManager/Properties/AssemblyInfo.cs
@@ -1,19 +1,6 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Alba.CsConsoleFormat.Sample.ProcessManager")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Alba.CsConsoleFormat.Sample.ProcessManager")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("99a14c98-3634-47fc-8919-975fc3757b57")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
